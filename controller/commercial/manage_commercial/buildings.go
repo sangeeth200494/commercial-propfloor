35106e2360970be7c2bac898d8d85c7e131c9ae2
sangeeth jayaraj <sangeethvv554@gmail.com>
2023-04-12 08:35:12 +0530

    buildings.go,location.go everything ok

--- controller/commercial/manage_commercial/buildings.go
+++ controller/commercial/manage_commercial/buildings.go
@@ -22,45 +22,34 @@ import (
 
 func SaveBasicBuildingDetails() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		name := c.PostForm("name")
 		location := c.PostForm("location")
-		availablity_for := c.PostForm("availability_for")
+		availability_for := c.PostForm("availability_for")
 		completion_status := c.PostForm("completion_status")
 		furnishing_status := c.PostForm("furnishing_status")
 		no_of_floor := c.PostForm("no_of_floor")
 		parking := c.PostForm("parking")
-		oc := c.PostForm("oc")
 		lift := c.PostForm("lift")
+		oc := c.PostForm("oc")
 		overlooking := c.PostForm("overlooking")
-
-		InsertBasicBuildingDetailInDB(name, location, availablity_for, completion_status, furnishing_status, no_of_floor, parking, oc, lift, overlooking)
-
+		InsertBasicBuildingDetailInDB(name, location, availability_for, completion_status, furnishing_status, no_of_floor, parking, lift, oc, overlooking)
 	}
 }
-
-func InsertBasicBuildingDetailInDB(name string, location string, availablity_for string, completion_status string,
-	furnishing_status string, no_of_floor string, parking string, oc string, lift string, overlooking string) (id int64) {
+func InsertBasicBuildingDetailInDB(name string, location string, availability_for string, completion_status string, furnishing_status string, no_of_floor string, parking string, lift string, oc string, overlooking string) (id int64) {
 	godotenv.Load()
 	client, _, _, _ := database.Mongoconnect(os.Getenv("MONGODB_HOST"))
+	fmt.Println(os.Getenv("APP_NAME"))
 	collection := client.Database("building_details").Collection("commertial_building")
-	doc := models.Property{Name: name, Location: location, AvailabilityFor: availablity_for, CompletionStatus: completion_status,
-		FurnishingStatus: furnishing_status, Floors: no_of_floor, Parking: parking, Oc: oc, Lift: lift, OverLooking: overlooking}
-
-	pattern := regexp.MustCompile("^[A-Za-z]*$")
+	doc := models.Property{Name: name, Location: location, AvailabilityFor: availability_for, CompletionStatus: completion_status, FurnishingStatus: furnishing_status, Floors: no_of_floor, Parking: parking, Lift: lift, Oc: oc, OverLooking: overlooking}
+	pattern := regexp.MustCompile("^[a-zA-Z]*$")
 	pattern2 := regexp.MustCompile("^[1-9][0-9]*$")
-
 	validate := validator.New()
 	err := validate.Struct(doc)
-	//var  string
-	//fmt.Scanln(&doc)
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
 			fmt.Println(err.Field(), err.Tag())
-			fmt.Println("ankit")
 		}
-
-	} else if pattern.MatchString(doc.Name) && pattern.MatchString(doc.Location) && pattern.MatchString(doc.AvailabilityFor) && pattern.MatchString(doc.CompletionStatus) && pattern.MatchString(doc.FurnishingStatus) && pattern.MatchString(doc.Parking) && pattern.MatchString(doc.Oc) && pattern.MatchString(doc.Lift) && pattern.MatchString(doc.OverLooking) && pattern2.MatchString(doc.Floors) {
+	} else if pattern2.MatchString(doc.Floors) && pattern.MatchString(doc.Name) && pattern.MatchString(doc.AvailabilityFor) && pattern.MatchString(doc.Location) && pattern.MatchString(doc.CompletionStatus) && pattern.MatchString(doc.FurnishingStatus) && pattern.MatchString(doc.Parking) && pattern.MatchString(doc.Lift) && pattern.MatchString(doc.Oc) && pattern.MatchString(doc.OverLooking) {
 		res, errr := collection.InsertOne(context.Background(), doc)
 		if errr != nil {
 			log.Fatal(errr)
@@ -71,7 +60,6 @@ func InsertBasicBuildingDetailInDB(name string, location string, availablity_for
 	} else {
 		fmt.Println("invalid input")
 	}
-
 	fmt.Println(os.Getenv("APP_NAME"))
 	return
 }
